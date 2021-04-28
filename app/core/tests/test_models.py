7132f266414c31645d0729dbf77480133105087d
Shimona Carvalho <shimona@truss.works>
2021-04-27 17:38:21 -0700

    Added user model tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,24 @@ class ModelTests(TestCase):
         """Test creating a new user with an email is successful"""
         email = "Robin@example.com"
         password = "Testpass123"
-        user = get_user_model().objects.create_user(email=email,
-                                                    password=password)
+        user = get_user_model().objects.create_user(email=email, password=password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = "test@LONDONappDEV.com"
+        user = get_user_model().objects.create_user(email, "test123")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "test123")
+
+    def test_create_new_super_user(self):
+        """Test creating a new super user"""
+        user = get_user_model().objects.create_super_user("test@example.com", "test123")
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
