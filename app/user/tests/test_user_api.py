a32fcf281d71e61eeb550df2578c1c7aa2c892e4
Shimona Carvalho <shimona@gmail.com>
2021-06-21 09:26:57 -0700

    user app tests added

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,60 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Test the users API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating user with valid payload is successful"""
+        payload = {
+            'email': 'shimona@example.com',
+            'password': 'pass1234',
+            'name': 'Candy Lauper',
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_mode().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+
+    def test_user_exists(self):
+        """Test creating a user that already exists"""
+        payload = {
+            'email': 'shimona@example.com',
+            'password': 'pass1234',
+            'name': 'Candy Lauper',
+        }
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test that the password must be more than 5 characters"""
+        payload = {
+            'email': 'shimona@example.com',
+            'password': 'pw123',
+            'name': 'Candy Lauper',
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
